fd040f34d704db79b8d6b017a30813ae06e375a6
Oz <spam@ozharrison.me>
2022-05-10 15:49:53 +0100

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    BookPageComponent
   ],
   imports: [
     BrowserModule,
